afd2015c7dc12860cf0a3b13cef9b1bcac6fc5bf
Gonzacaser <gonzacasermeiro@gmail.com>
2022-06-30 23:37:37 -0300

    Agregue mas casos para digitos en URL

--- tests/Feature/CorrectDigitsUrlTest.php
+++ tests/Feature/CorrectDigitsUrlTest.php
@@ -12,46 +12,61 @@ class CorrectDigitsUrlTest extends TestCase
      *
      * @return void
      */
-    public function test_one_digit_url()
+    public function test_One_digit_url()
     {
         $response = $this->get('/jugar/1');
         $response->assertStatus(200);
     }
     
     
-    public function test_two_digits_url()
+    public function test_OneToTwo_digits_url()
     {
         $response = $this->get('/jugar/12');
         $response->assertStatus(200);
     }
 
 
-    public function test_three_digits_url()
+    public function test_OneToThree_digits_url()
     {
         $response = $this->get('/jugar/123');
         $response->assertStatus(200);
     }
 
 
-    public function test_four_digits_url()
+    public function test_OneToFour_digits_url()
     {
         $response = $this->get('/jugar/1234');
         $response->assertStatus(200);
     }
 
 
-    public function test_five_digits_url()
+    public function test_OneToFive_digits_url()
     {
         $response = $this->get('/jugar/12345');
         $response->assertStatus(200);
     }
 
 
-    public function test_six_digits_url()
+    public function test_OneToSix_digits_url()
     {
         $response = $this->get('/jugar/123456');
         $response->assertStatus(200);
     }
+
+    
+    public function test_OneToSeven_digits_url()
+    {
+        $response = $this->get('/jugar/1234567');
+        $response->assertStatus(200);
+    }
+
+
+    public function test_OneToEight_digits_url()
+    {
+        $response = $this->get('/jugar/12345678');
+        $response->assertStatus(200);
+    }
+
 
     public function test_digit_eight_in_url()
     {
@@ -72,4 +87,5 @@ class CorrectDigitsUrlTest extends TestCase
         $response = $this->get('/jugar/a');
         $response->assertStatus(200);
     }
+    
 }
