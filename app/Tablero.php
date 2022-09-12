f6ae95fe8226b07367a511ed2c9330ce46f7ab75
gonzacaser <gonzacasermeiro@gmail.com>
2022-09-12 00:22:20 -0300

    Completada clase Tablero

--- app/Tablero.php
+++ app/Tablero.php
@@ -5,6 +5,10 @@ namespace App;
 interface tableroInterface {
     public function limpiarTablero();
     public function mostrarTablero();
+    public function alturaEnJuego(): int;
+    public function anchuraEnJuego(): int;
+    public function checkearCasilla(int $xPosicion, int $yPosicion);
+    public function tirarFicha(int $xPosicion, Ficha $ficha);
 
 }
 
@@ -14,6 +18,7 @@ class Tablero implements tableroInterface {
     protected int $alto;
     public array $tablero;
 
+//  lleno el tablero de fichas blancas para limpiarlo
     public function limpiarTablero() {
 
         for ($x=0; $x<$this->ancho; $x++) {
@@ -23,15 +28,19 @@ class Tablero implements tableroInterface {
         }
     }
 
+
     public function __construct(int $ancho, int $alto) {
 
+        //if ($ancho < 4 && $alto < 4){
+        //    throw expection }
+        
         $this->ancho = $ancho;
         $this->alto = $alto;
         $this->limpiarTablero();
 
     }
 
-    public function mostrarTablero(){
+    public function mostrarTablero() {
 
         echo "\n" . str_repeat('~', $this->ancho) . "\n";
         echo "Este es el tablero del juego";
@@ -45,7 +54,6 @@ class Tablero implements tableroInterface {
                 echo $i . "\t";
             }
 
-
         }
 
         for($o=0; $o<$this->ancho; $o++){
@@ -53,10 +61,37 @@ class Tablero implements tableroInterface {
             for($k=0; $k<$this->alto; $k++){
                 echo $this->tablero[$o][$k]->getColour() . "\t";
             }
-        
         }
+    }
+
+    // hago getters para obtener alto y ancho en clase ganar
+    public function alturaEnJuego(): int {
+        return $this->alto;
+    }
     
+    public function anchuraEnJuego(): int {
+        return $this->ancho;
+    }
+
+    // verifico si hay algo en la casilla
+    public function checkearCasilla(int $xPosicion, int $yPosicion){
+        return (($this->tablero[$xPosicion][$yPosicion]->getColour() == 'rojo') || ($this->tablero[$xPosicion][$yPosicion]->getColour() == 'azul'));
+
+    }
     
+    public function tirarFicha(int $xPosicion, Ficha $ficha) {
+        //if ($xPosicion < 0 || $xPosicion > $this->ancho)
+        // exception
+    
+        for($x=$this->alto; $x>=0 ;$x--){
+
+            if(!$this->checkearCasilla($xPosicion, $x)) {
+                
+                $this->tablero[$xPosicion][$x] = $ficha;
+                break;
+            }
+
+        }
     }
 }
 
